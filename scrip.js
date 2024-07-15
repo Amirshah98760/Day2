7fecffa20b9aadbfd30163488a43f44da15ade5d
Amirshah98760 <amirshah98760@gmail.com>
2024-07-15 23:03:44 +0500

    Chai aur code Day 2 code

---
+++ scrip.js
@@ -0,0 +1,144 @@
+// Activity 1: Arthemetic Operations
+// Task 1:
+
+const a = 20;
+const b = 45;
+console.log('The addition of a + b is = ' , a+b);
+
+// Task 2:
+const c = 45;
+const d = 20;
+console.log('The subtraction of c - d is = ' , c - d);
+
+// Task 3:
+
+const  num1= 5;
+const num2 = 10;
+console.log('The Multiplicaiton of two number is = ' ,  num1 * num2);
+
+// Task 4:
+const  num3= 20;
+const num4 = 5;
+console.log('The division of two number  is = ' ,  num3 / num4);
+
+const  num5= 21;
+const num6 = 5;
+console.log('The Remainder of two number  is = ' ,  num5 % num6);
+
+// Activity no 3
+// Task 1:
+let myNumber = 40;
+
+console.log('Initial value = ' + myNumber);
+
+myNumber += 5;
+
+console.log('After adding the value 5 = ',  myNumber);
+
+// Task 2:
+let myNumber1 = 40;
+
+console.log('Initial value = ' + myNumber1);
+
+myNumber1 -= 5;
+
+console.log('After subtract the value 5 = ',  myNumber1);
+
+// Activity 3:Comparison Operators
+// Task 8:
+// let number5 = 40;
+// let number6 = 30;
+
+// if (number5 > number6) {
+//     console.log(number5 + ' is greater than '+ number6);
+// }
+// else if(number5 < number6)
+// {
+//     console.log(number5 + ' is less than '+ number6)
+// }
+
+// else{
+//     console.log(number5 + ' is equal to '+number6)
+// }
+
+// Task 9:
+
+// let number1 = 25;
+// let number2 = 25;
+
+// // Compare the numbers using >= and <= and log the results
+// if (number1 >= number2) {
+//     console.log(number1 + " is greater than or equal to " + number2);
+// } else {
+//     console.log(number1 + " is less than " + number2);
+// }
+
+// if (number1 <= number2) {
+//     console.log(number1 + " is less than or equal to " + number2);
+// } else {
+//     console.log(number1 + " is greater than " + number2);
+// }
+
+
+// Task 10:
+
+// let number6 = 20;
+// let number7= "20";
+
+// if (number6 == number7) {
+//     console.log(number6 + ' is equal to  '+ number7);
+// }
+
+// if (number6 === number7) {
+//     console.log(number6 + ' is equal to  '+ number7);
+// }
+// else{
+//     console.log(number6 +' is not equal to '+number7 + ' beacause the number1 type is number and the number2 type is string and === requires both the type and the value to be the same for the comparison to return true ')
+// }
+
+
+// Task 10 : Program 2 
+
+// console.log(5=='5'); // true because does not check the type
+// console.log(5==='5'); // false because checks the value and type
+
+// Activity 4: Logical Operator
+// Task 11:
+
+// let marks = 90;
+// if (marks >= 90 && marks < 100) {
+//     console.log('Your gpa is ')
+// }
+
+// Task 12:
+ // Declare two variables and assign them values
+// let age = 20;
+// let hasPermission = false;
+
+// // Use the || operator to combine two conditions
+// if (age >= 18 || hasPermission) {
+//     console.log("Access granted.");
+// } else {
+//     console.log("Access denied.");
+// }
+
+// Task 13:
+
+// let age = 13;
+
+// if (age !== 18) {
+//     console.log('You are not  attent the studio')
+// }
+
+
+// Activity 5:Ternary Operator
+// Task 14:
+
+// Declare a variable and assign it a value
+let number = 10;
+
+// Use the ternary operator to check if the number is positive or negative
+let result = (number >= 0) ? "The number is positive" : "The number is negative";
+
+// Log the result to the console
+console.log(result);
